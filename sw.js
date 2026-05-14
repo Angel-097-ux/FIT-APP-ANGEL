713f2ce1a9664234499b6eeb7f800758ffadfd99
Tu Nombre <tu@email.com>
2026-05-13 22:30:21 -0300

    fix: service worker sin cache agresivo

--- sw.js
+++ sw.js
@@ -1,12 +1,1 @@
-const CACHE = 'fit21-v1';
-const FILES = ['./index.html', './manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
+const CACHE = "fit21-v2"; self.addEventListener("install", e=>{ self.skipWaiting(); }); self.addEventListener("activate", e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k))))); }); self.addEventListener("fetch", e=>{ e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))); });
